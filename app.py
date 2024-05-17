775c358dadeb4b985df18d56d01a33458dff05d5
randomstuff11 <168477251+randomstuff11@users.noreply.github.com>
2024-05-17 09:59:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, render_template, jsonify, redirect, url_for
 import pyodbc
 import pandas as pd
 import sqlite3
-from datetime import datetime
 
 app = Flask(__name__)
 
@@ -11,39 +10,42 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
     conn = get_db_connection()
     queries = conn.execute('SELECT * FROM all_queries ORDER BY timestamp DESC LIMIT 10').fetchall()
+    connections = conn.execute('SELECT * FROM connections').fetchall()
     conn.close()
-    return render_template('index.html', queries=queries)
+    return render_template('index.html', queries=queries, connections=connections)
+
+@app.route('/manage_connections', methods=['GET', 'POST'])
+def manage_connections():
+    if request.method == 'POST':
+        name = request.form['name']
+        connection_string = request.form['connection_string']
+        conn = get_db_connection()
+        conn.execute('INSERT INTO connections (name, connection_string) VALUES (?, ?)', (name, connection_string))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('index'))
+    return render_template('manage_connections.html')
 
 @app.route('/query', methods=['POST'])
 def query():
     sql_query = request.form['query']
-    connection_string = request.form['connection']
-    conn_odbc = pyodbc.connect(connection_string)
+    connection_id = int(request.form['connection'])
+    conn = get_db_connection()
+    connection = conn.execute('SELECT connection_string FROM connections WHERE id = ?', (connection_id,)).fetchone()
+    conn_odbc = pyodbc.connect(connection['connection_string'])
 
     df = pd.read_sql(sql_query, conn_odbc)
     results = df.to_html()
 
-    # Save query to database
-    conn = get_db_connection()
-    conn.execute('INSERT INTO all_queries (query, connection) VALUES (?, ?)', (sql_query, connection_string))
+    conn.execute('INSERT INTO all_queries (query, connection_id) VALUES (?, ?)', (sql_query, connection_id))
     conn.commit()
     conn.close()
 
-    return render_template('results.html', results=results, query=sql_query, connection=connection_string)
-
-@app.route('/save_query', methods=['POST'])
-def save_query():
-    sql_query = request.form['query']
-    connection_string = request.form['connection']
-    conn = get_db_connection()
-    conn.execute('INSERT INTO saved_queries (query, connection) VALUES (?, ?)', (sql_query, connection_string))
-    conn.commit()
-    conn.close()
-    return redirect(url_for('index'))
+    return render_template('results.html', results=results)
 
 if __name__ == '__main__':
     app.run(debug=True)
